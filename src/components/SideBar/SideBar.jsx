37823c545e12b355a5e800c6d5ebac14459d594a
TrunkNq <nqtrung0810@gmail.com>
2024-01-25 10:22:47 +0700

    update code

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -6,7 +6,7 @@ const SideBar = () => {
 		<Col lg={3} className="bg-dark link-primary">
 			<ul>
 				<li>Dashboard</li>
-				<li>Users</li>
+				<li>Users---</li>
 				<li>Tours--</li>
 				<li>Orders</li>
 			</ul>
